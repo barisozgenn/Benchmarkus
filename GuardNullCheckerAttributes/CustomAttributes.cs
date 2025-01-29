d97b0597194d201a801a3eb6b55a7385a81fff63
barisozgenn <48822802+barisozgenn@users.noreply.github.com>
2025-01-29 23:17:19 +0300

    refactor Check NullValue With Ardalis Attribute

--- GuardNullCheckerAttributes/CustomAttributes.cs
+++ GuardNullCheckerAttributes/CustomAttributes.cs
@@ -2,22 +2,22 @@ using System;
 namespace GuardNullCheckerAttributes;
 
 /// <summary>
-    /// Indicates this method's parameters should be checked for null
-    /// using Ardalis.GuardClauses before the method body executes.
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Method)]
-    public class CheckNullValueWithArdalisAttribute : Attribute
-    {
-        // For demonstration only.
-        // In real scenarios, you might rely on AOP or source generators.
-    }
+/// Indicates this method's parameters should be checked for null
+/// using Ardalis.GuardClauses before the method body executes.
+/// </summary>
+[AttributeUsage(AttributeTargets.Method)]
+public class CheckNullValueWithArdalisAttribute : Attribute
+{
+    // For demonstration only.
+    // In real scenarios, you might rely on AOP or source generators.
+}
 
-    /// <summary>
-    /// Indicates this method's parameters should be checked for null
-    /// using a basic 'if' statement before the method body executes.
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Method)]
-    public class CheckNullValueWithoutArdalisAttribute : Attribute
-    {
-        // Similarly for demonstration.
-    }
+/// <summary>
+/// Indicates this method's parameters should be checked for null
+/// using a basic 'if' statement before the method body executes.
+/// </summary>
+[AttributeUsage(AttributeTargets.Method)]
+public class CheckNullValueWithoutArdalisAttribute : Attribute
+{
+    // Similarly for demonstration.
+}
